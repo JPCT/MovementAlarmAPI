e7225b3ee9d99b2deeb6b05c87dbfff6bf443263
Lucas <lucas.1701716343@gmail.com>
2021-12-12 03:02:41 -0500

    Git init

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,128 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace MovementAlarmAPI
+{
+    [ApiController]
+    [Route("api/notification")]
+    public class NotificationController : ControllerBase
+    {
+        public NotificationController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string x = "Hola Mundo";
+            return x;
+        }
+
+        [HttpPost("notify")]
+        public string NotifyMovement(string messageContent, string imagePath, string imageName)
+        {
+            var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+            TwilioClient.Init(accountSid, authToken);
+
+            //string fileId = UploadImageToDriveAsync("Images\\test.png", "test2.png").Result;
+            string fileId = UploadImageToDriveAsync(imagePath, imageName).Result;
+
+            var message = MessageResource.Create(
+                body: messageContent,
+                mediaUrl: new List<Uri> { new Uri("https://drive.google.com/uc?export=view&id=" + fileId) },
+                from: new PhoneNumber("whatsapp:+14155238886"),
+                to: new PhoneNumber("whatsapp:+573136367416")
+            );
+
+            return message.Status.ToString();
+        }
+
+        [HttpGet("upload")]
+        public async Task<string> UploadImageToDriveAsync(string imagePath, string imageName)
+        {
+            UserCredential credential;
+            string[] Scopes = { DriveService.Scope.Drive
+            };
+
+            using (var stream1 =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream1).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "MovementAlarmAPI",
+            });
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = imageName,
+                Parents = new List<string>() { }
+            };
+
+            string uploadedFileId;
+            // Create a new file on Google Drive
+            var root = Directory.GetCurrentDirectory();
+            var path = Path.Combine(root, imagePath);
+
+            await using (var fsSource = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                // Create a new file, with metadata and stream.
+                var request = service.Files.Create(fileMetadata, fsSource, "image/png");
+                request.Fields = "*";
+                var results = await request.UploadAsync(CancellationToken.None);
+
+                if (results.Status == UploadStatus.Failed)
+                {
+                    Console.WriteLine($"Error uploading file: {results.Exception.Message}");
+                }
+
+                // the file id of the new file we created
+                uploadedFileId = request.ResponseBody?.Id;
+            }
+
+            var file = service.Files.Get(uploadedFileId);
+            Permission permi = new Permission();
+            permi.Type = "anyone";
+            permi.Role = "writer";
+            try
+            {
+                var x = service.Permissions.Create(permi, uploadedFileId).Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return uploadedFileId;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using colegal;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace MovementAlarmAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+    }
+}
